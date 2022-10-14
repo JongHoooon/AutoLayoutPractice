6edfb0dc9095126b6c4a4a82d499ecd1bb0536ed
seaLion3 <qnm541@gmail.com>
2022-10-14 13:30:07 +0900

    first commit 10-2-home

---
+++ 10-2-home/10-2-home/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10-2-home
+//
+//  Created by JongHoon on 2022/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
